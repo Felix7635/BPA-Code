d14d1a0f282921b72a9f22d050607d4463d54e4d
Felix7635 <77011900+Felix7635@users.noreply.github.com>
2021-06-08 16:44:15 +0200

    Funktion zum Einstellen des npc's hinzugefügt

--- Core/Inc/settings.h
+++ Core/Inc/settings.h
@@ -2,6 +2,7 @@
 
 void settings_lcd(Lcd_HandleTypeDef *lcd);
 void settings_led(Lcd_HandleTypeDef *lcd);
+void settings_npc(Lcd_HandleTypeDef *lcd);
 
 uint8_t read_settings();
 uint8_t write_settings();
--- Core/Src/settings.c
+++ Core/Src/settings.c
@@ -25,6 +25,86 @@ void settings_led(Lcd_HandleTypeDef *lcd)
 	change_pwm(lcd, SET_LED);
 }
 
+void settings_npc(Lcd_HandleTypeDef *lcd)
+{
+	Lcd_clear(lcd);
+	enc_position = Univers.newpacketcharacter;
+	uint8_t temp = Univers.newpacketcharacter, tempx = Univers.exchangecharacter;
+	uint8_t position = 0;
+	char arrow = 0x5E;
+
+	Lcd_cursor(lcd, 0, 4);
+	Lcd_string_length(lcd, "NPC aendern", 11);
+	Lcd_cursor(lcd, 3, 0);
+	Lcd_string_length(lcd, &arrow, 1);
+	temp--;
+
+	while(!Button_pressed(BACK))
+	{
+		if(enc_position < 0)
+			enc_position = 0;
+		else if(enc_position > 255)
+			enc_position = 255;
+
+		if(enc_position != (position == 0 ? temp : tempx))
+		{
+			if(position == 0)
+				temp = enc_position;
+			else if(position == 1)
+				tempx = enc_position;
+
+			Lcd_cursor(lcd, 2, 0);
+			Lcd_int(lcd, temp);
+			Lcd_string_length(lcd, "   ", 2);
+			Lcd_cursor(lcd, 2, 4);
+			Lcd_string_length(lcd, "ersetzen mit ", 13);
+			Lcd_int(lcd, tempx);
+			Lcd_string_length(lcd, "  ", 2);
+		}
+
+		if(Button_pressed(UP) && position == 1)
+		{
+			position = 0;
+			enc_position = temp;
+			Lcd_cursor(lcd, 3, 0);
+			Lcd_string_length(lcd, &arrow, 1);
+			Lcd_cursor(lcd, 3, 17);
+			Lcd_string_length(lcd, " ", 1);
+		}
+		else if(Button_pressed(DOWN) && position == 0)
+		{
+			position++;
+			enc_position = tempx;
+			Lcd_cursor(lcd, 3, 17);
+			Lcd_string_length(lcd, &arrow, 1);
+			Lcd_cursor(lcd, 3, 0);
+			Lcd_string_length(lcd, " ", 1);
+		}
+
+		if(Button_pressed(ENTER))
+		{
+			if(temp != tempx)
+			{
+				Univers.newpacketcharacter = temp;
+				Univers.exchangecharacter = tempx;
+				write_settings();
+				Lcd_clear(lcd);
+				Lcd_cursor(lcd, 1, 3);
+				Lcd_string_length(lcd, "NPC geaendert", 13);
+				while(!Button_pressed(ENTER));
+				return;
+			}
+			else
+			{
+				Lcd_clear(lcd);
+				Lcd_string_length(lcd, "Fehler", 6);
+				while(!Button_pressed(BACK));
+			}
+		}
+	}
+	return;
+}
+
 void change_pwm(Lcd_HandleTypeDef *lcd, uint8_t mode)
 {
 	volatile uint32_t *regi;
@@ -90,6 +170,7 @@ uint8_t read_settings()
 	f_read(&setting_file, &tmp, 4, &bytesread);
 	htim1.Instance->CCR4 = tmp;
 	f_read(&setting_file, &Univers.newpacketcharacter, 1, &bytesread);
+	f_read(&setting_file, Univers.exchangecharacter, 1, &bytesread);
 	f_close(&setting_file);
 	return 1;
 }
@@ -108,6 +189,7 @@ uint8_t write_settings()
 	tmp = htim1.Instance->CCR4;
 	f_write(&setting_file, &tmp, 4, &byteswritten);		//LCD
 	f_write(&setting_file, &Univers.newpacketcharacter, 1, &byteswritten);	//New Packet Character
+	f_write(&setting_file, Univers.exchangecharacter, 1, &byteswritten);
 	f_close(&setting_file);
 	return 1;
 }
